79cdad3c050cfa2f7293642e6e9ddcd66fbc576f
69PercentFat <shubham11501@gmail.com>
2023-11-30 12:37:17 +0530

    Concatenation

---
+++ day2.dart
@@ -0,0 +1,14 @@
+void main() {
+  String greeting = 'hello, world';
+
+  print(greeting);
+
+  greeting = '$greeting  yooo';
+  print(greeting);
+
+  greeting = '$greeting hey man \$12';
+  print(greeting);
+
+  greeting = '$greeting \n hey man \$12';
+  print(greeting);
+}
