59edf451e49f07e1abfe68de3e110299361774a7
69PercentFat <shubham11501@gmail.com>
2023-11-30 14:26:20 +0530

    Reassigning variables and concatenation

--- day2.dart
+++ day2.dart
@@ -11,4 +11,19 @@ void main() {
 
   greeting = '$greeting \n hey man \$12';
   print(greeting);
+
+  // New way to create a variable
+
+  // var/final/const variableName = value;
+  // var can be asigned multiple times, but const and final cant be reasignes
+
+  var someValue = 10;
+  print(someValue);
+  print(someValue.bitLength);
+  var someValue2 = 'hey man';
+  print(someValue2);
+  print(someValue2.length);
+
+  final todayTime = DateTime.now();
+  print(todayTime);
 }
