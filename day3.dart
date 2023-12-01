0446f47c7c4f101faaab06caf70ce9b3d8b965fc
69PercentFat <shubham11501@gmail.com>
2023-12-01 18:50:46 +0530

    day 3

---
+++ day3.dart
@@ -0,0 +1,18 @@
+void main() {
+  final someValue = DateTime.now();
+  const someValue2 = 10;
+
+  print(someValue);
+  print(someValue2);
+
+  // Optional variables
+  // string/int/bool and null
+
+  String? firstValue = '';
+
+  print(firstValue);
+
+  int? some = null;
+
+  print(some);
+}
