a921eabd10a76ec84569f05e8540062c3b65711a
69PercentFat <shubham11501@gmail.com>
2023-12-06 12:50:00 +0530

    Day 4

---
+++ day4.dart
@@ -0,0 +1,15 @@
+// Control Flow
+
+void main() {
+  //if statements
+
+  int age = 12;
+
+  if (age == 120) {
+    print('dead');
+  } else if (age >= 18) {
+    print('adult');
+  } else {
+    print('child');
+  }
+}
